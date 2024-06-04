8d7af7d217b9711b444734fa12ec9516cc535150
Charlie Quick <melvinquick@proton.me>
2024-06-04 10:53:28 -0400

    Trying to check for each key container existance before moving to the next part of the path

--- driver-scripts/disable-windows-copilot.ps1
+++ driver-scripts/disable-windows-copilot.ps1
@@ -5,6 +5,17 @@ $registry_path = "HKCU\SOFTWARE\Policies\Microsoft\Windows\Windowscopilot"
 $value_name = "TurnOffWindowscopilot"
 $value_data = 1
 $property_type = "DWORD"
+$key_paths = $registry_path.Split('\')
+
+foreach ($key_path in $key_paths){
+    $full_path = Join-Path -Path ($key_paths[0..($key_paths.IndexOf($key_path) - 1)]) -ChildPath $key_path
+
+    if (!(Test-Path -Path $full_path -PathType Container)){
+        New-Item -Path $full_path -Force | Out-Null
+    }
+
+    $key_paths = $full_path
+}
 
 New-ItemProperty -Path $registry_path -Name $value_name -Value $value_data -PropertyType $property_type -Force
 
